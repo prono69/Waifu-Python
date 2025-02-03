c5bda0c5455edd34a54916a4274df1742404b3de
misfit <akoushik88@gmail.com>
2025-02-03 17:44:32 +0530

    initial5

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ long_description = (Path(__file__).parent / "README.MD").read_text()
 
 setup(
     name="waifu-python",
-    version="1.1",
+    version="1.2",
     packages=find_packages(),
     install_requires=["httpx"],
     author="Misfit",
     description="A project born out of boredom, designed to simplify and reduce the code related to the Waifu API.",
-    url="https://github.com/yourusername/Waifu-Python",
+    url="https://github.com/MisfiT2020/Waifu-Python",
     license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
